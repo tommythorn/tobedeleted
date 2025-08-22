fb8ef21274a5474d22b8259f0db0173bed052346
Tommy Thorn <tommy-github2@thorn.ws>
2025-08-22 08:50:39 -0700

    Silly test

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_example_tommythorn (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,28 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+   // All output pins must be assigned. If not used, assign to 0.
+   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+   assign uio_out = 0;
+   assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+   // List all unused inputs to prevent warnings
+   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+   reg [63:0] rf[4:0];
+   reg [4:0]  addr;
+   reg [63:0] data;
+
+   assign uo_out[0] = data[63];
+
+   always @(posedge clk) begin
+      if (ui_in[1])
+	data <= rf[addr];
+      else if (ui_in[2])
+	rf[addr] <= data;
+      else
+	{data,addr} <= {data,addr} << 1 | ui_in[0];
+      if (!rst_n)
+	{data,addr} <= 0;
+   end
 endmodule
